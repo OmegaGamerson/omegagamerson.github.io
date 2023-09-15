1ffe32d15044a2c8ea99b49350691842932a7322
OmegaGamerson <124854718+OmegaGamerson@users.noreply.github.com>
2023-09-14 22:36:39 -0400

    Update

--- ProxyAPI.js
+++ ProxyAPI.js
@@ -2,7 +2,7 @@ const urlToFetch = 'https://example.com'; // Replace with the URL you want to fe
 fetch(`http://your-server-address:3000/proxy?url=${encodeURIComponent(urlToFetch)}`)
   .then(response => response.text())
   .then(data => {
-    // Handle and display the fetched data on your website
+    
     console.log(data);
   })
   .catch(error => {
